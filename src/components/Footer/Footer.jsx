365e87b3b592ed36c48d9fe25472ae128be5dec9
Jordon Cunningham <153477169+JAC874@users.noreply.github.com>
2024-11-20 14:52:29 -0500

    update footer elements and styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,48 +5,48 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer__copyright">
-        © 2024 Supersite, Powered by News API
-      </div>
-      <div className="footer__links-container">
-        <Link to="/" className="footer__link footer__link__home">
-          <p className="footer__home">Home</p>
-        </Link>
-        <a
-          href="https://tripleten.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer__link footer__link__tripleten"
-        >
-          TripleTen
-        </a>
-        <a
-          href="https://github.com/JAC874"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer__link footer__link__github"
-        >
-          <img
-            src={github}
-            alt="GitHub Profile Icon"
-            className="footer__icon"
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jordoncunningham/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer__link footer__link__linkedin"
-        >
-          <img
-            src={linkedin}
-            alt="LinkedIn Profile Icon"
-            className="footer__icon"
-          />
-        </a>
-      </div>
-    </div>
+    <footer className="footer">
+      <p className="footer__copyright">© 2024 Supersite, Powered by News API</p>
+      <nav className="footer__nav">
+        <ul className="footer__links">
+          <Link to="/" className="footer__link footer__link__home">
+            <p className="footer__home">Home</p>
+          </Link>
+          <a
+            href="https://tripleten.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__link footer__link__tripleten"
+          >
+            TripleTen
+          </a>
+          <a
+            href="https://github.com/JAC874"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__link footer__link__github"
+          >
+            <img
+              src={github}
+              alt="GitHub Profile Icon"
+              className="footer__icon"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jordoncunningham/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__link footer__link__linkedin"
+          >
+            <img
+              src={linkedin}
+              alt="LinkedIn Profile Icon"
+              className="footer__icon"
+            />
+          </a>
+        </ul>
+      </nav>
+    </footer>
   );
 }
 
